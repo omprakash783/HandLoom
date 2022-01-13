1313a44d4e267c6193f1723b400fd76ed376c7ca
omprakash783 <omprakashbhi4761@gmail.com>
2022-01-13 14:30:06 +0530

    added shop page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react'
-import HomePage from '../src/pages/HomePage/homepage.component'
+import {Routes,Route} from 'react-router-dom'
+import HomePage from './pages/HomePage/homepage.component'
+import ShopPage from './pages/Shop/shop.component'
+
+function Hat(){
+  return(
+  <div>
+    <h1>Hats</h1>
+  </div>)
+}
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+      <Routes>
+          <Route exact path='/' element={<HomePage />} />
+          <Route path='/shop' element={<ShopPage />} />
+          <Route path='/hats' element={<Hat />}  />
+      </Routes>
+    
   );
 }
 
